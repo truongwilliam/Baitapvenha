49bdb3ca69af1e9ae3c0ac031fde3f043d019780
truomgwilliam <thcong2k5@gmail.com>
2024-11-04 20:47:18 +0700

    init project

---
+++ src/timsonguyento/TimSoNguyenTo.java
@@ -0,0 +1,28 @@
+
+package timsonguyento;
+
+public class TimSoNguyenTo {
+    
+    public static boolean isPrime(int n) {   
+        if (n <= 1) {  
+            return false;
+        }  
+        for (int i = 2; i <= Math.sqrt(n); i++) {  
+            if (n % i == 0) {  
+                return false;  
+            }  
+        }  
+        return true;  
+    }  
+
+    public static void main(String[] args) {
+        System.out.println("Cac so nguyen to tu 1 den 100 la:");  
+        for (int num = 1; num <= 100; num++) {  
+            if (isPrime(num)) {  
+                System.out.print(num + " "); 
+            }  
+        }  
+    }  
+}
+    
+
